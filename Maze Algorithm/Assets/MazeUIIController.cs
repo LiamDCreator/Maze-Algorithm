682c7db46c6adc9d40183afb48f1785ea9825359
LiamDCreator <Liamduijn@gmail.com>
2025-06-29 21:08:53 +0200

    fixed bug so input field doesn't instantly change width or height value

--- Maze Algorithm/Assets/MazeUIIController.cs
+++ Maze Algorithm/Assets/MazeUIIController.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MazeUIController : MonoBehaviour
+public class MazeUIIController : MonoBehaviour
 {
     public MazeGenerator mazeGenerator;
     public InputField widthInput;
     public InputField heightInput;
     public InputField waitTimeInput;
 
+    private int tempWidth;
+    private int tempHeight;
+    private float tempWaitTime;
+
     void Start()
     {
-        // Initialize UI with current values
-        widthInput.text = mazeGenerator.mazeWidth.ToString();
-        heightInput.text = mazeGenerator.mazeHeight.ToString();
-        waitTimeInput.text = mazeGenerator.waitTime.ToString("F2");
+        tempWidth = mazeGenerator.mazeWidth;
+        tempHeight = mazeGenerator.mazeHeight;
+        tempWaitTime = mazeGenerator.waitTime;
 
-        // Add listeners
+        widthInput.text = tempWidth.ToString();
+        heightInput.text = tempHeight.ToString();
+        waitTimeInput.text = tempWaitTime.ToString("F2");
+
         widthInput.onEndEdit.AddListener(OnWidthChanged);
         heightInput.onEndEdit.AddListener(OnHeightChanged);
         waitTimeInput.onEndEdit.AddListener(OnWaitTimeChanged);
@@ -24,20 +30,27 @@ public class MazeUIController : MonoBehaviour
     public void OnWidthChanged(string value)
     {
         if (int.TryParse(value, out int width) && width > 0)
-            mazeGenerator.mazeWidth = width;
+            tempWidth = width;
     }
 
     public void OnHeightChanged(string value)
     {
         if (int.TryParse(value, out int height) && height > 0)
-            mazeGenerator.mazeHeight = height;
+            tempHeight = height;
     }
 
     public void OnWaitTimeChanged(string value)
     {
         if (float.TryParse(value, out float waitTime) && waitTime >= 0f)
-        {
-            mazeGenerator.waitTime = waitTime;
-        }
+            tempWaitTime = waitTime;
+    }
+
+    // Call this from your Generate button
+    public void ApplyMazeSettings()
+    {
+        mazeGenerator.mazeWidth = tempWidth;
+        mazeGenerator.mazeHeight = tempHeight;
+        mazeGenerator.waitTime = tempWaitTime;
+        mazeGenerator.GenerateMaze();
     }
 }
